e335a21a1eed707c976d0b94065c85b8e63f6fbc
Valdir Borges <501161@soa.ad.pwcinternal.com>
2019-08-06 12:23:17 -0300

    aula 12 completada

--- telusko/travello/views.py
+++ telusko/travello/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+
+    dest1 = Destination()
+    dest1.name = 'Mumbai'
+    dest1.desc = 'The City That never sleeps'
+    dest1.price = 700
+
+    return render(request, "index.html", { 'dest1': dest1 })
