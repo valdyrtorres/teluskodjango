76ce2c89f395db02dbb03f540a66c7aba069fec4
Valdir Borges <501161@soa.ad.pwcinternal.com>
2019-08-06 17:47:39 -0300

    Aula 18 completada

--- telusko/travello/models.py
+++ telusko/travello/models.py
@@ -7,5 +7,5 @@ class Destination(models.Model):
     name = models.CharField(max_length=100)
     img = models.ImageField(upload_to='pics')
     dest = models.TextField(0)
-    price = models.IntegerField
+    price = models.IntegerField()
     offer = models.BooleanField(default=False)
